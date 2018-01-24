349c3e05a72d9498bf5acc1c376b25889cbb683a
unknown <maximilien.bianchi@gmail.com>
2018-01-24 11:19:52 -0500

    simplifier 5_push_button.cpp avec nouvelle fonction buttonCondition() et rajouter documentation

--- tp3/5_push_button.cpp
+++ tp3/5_push_button.cpp
@@ -1,46 +1,66 @@
-/*
- * Nom: Flashy light program
- * Description: After 5 pushes of a button, LED turns on for 5 seconds then returns to standby state
- * Version: 2.1
- */
+/****************************************************************************************
+ * Nom:         Flashy light program
+ * Description: Si le bouton est pousser 5 fois, la DEL libre s'allume en ROUGE pour 1 seconde
+ * Utilisation: Brancher fil MAUVE sur PORTC-1 et BLANC sur PORTC-2. Ensuit brancher MAUVE sur borne + et BLANC sur la borne - de DEL libre
+ *              Pesez bouton 5 fois pour allumer DEL libre pour 1 seconde.
+ * Version:     2.1
+ ***************************************************************************************/
 
 
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
 
-int main() {
+/*Declaration des constantes*/
 
-    DDRC = 0xff; // PORT C est en mode sortie
-    DDRD = 0x00; // PORT D est en mode entree
+DDRC = 0xff;                        // PORT C est en mode sortie
+DDRD = 0x00;                        // PORT D est en mode entree
     
-    const short DEBOUNCETIME = 40;
-    const int ONTIME = 1000;
-    const uint8_t red = 0x01;
-    const uint8_t off - 0x00;
-    enum state {INIT, EA} s = INIT;             // INIT = Etat initial, EA = Etat allume
+const short DEBOUNCETIME = 40;      // 40 ms de antirebond
+const int ONTIME = 1000;            // 1000 ms d'attente
+const uint8_t red = 0x01;           // DEL couleur rouge
+const uint8_t off - 0x00;           // DEL eteinte
+
+/****************************************************************************************
+ * Fonction buttonCondition
+ ***************************************************************************************/
+
+void buttonCondition(&buttonIsPressed, &buttonWasPressed) {
     
+    buttonWasPressed = buttonIsPressed;   // buttonWasPressed prend la valeur de buttonIsPressed 
+                                          // pour empecher qu'on continue a augmenter le buttonPushCounter
     
+    if (PIND & 0x04) {                    // Si le bouton est pese ...
+        buttonIsPressed = true;
+        }
+    else {                                // Si le bouton n'est pas pese ...
+        buttonIsPressed = false;
+    }
+}
+
+/*****************************************************************************************
+ * Fonction main
+ ****************************************************************************************/
+
+int main() {
+    
+    /*Declaration et initialisation des variables*/
+    
+    enum state {INIT, EA} s = INIT;                           // INIT: Etat Initial, EA: Etat Allumer
     uint8_t buttonPushCounter = 0;
     bool buttonIsPressed = false;
     bool buttonWasPressed = false;
     
+    
     while(true) {
         
-        _delay_ms(DEBOUNCETIME);
+        _delay_ms(DEBOUNCETIME);                              // antirebond
         
-        buttonWasPressed = buttonIsPressed;
-        
-        if (PIND & 0x04) {
-            buttonIsPressed = true;
-        }
-        else {
-            buttonIsPressed = false;
-        }
+        buttonCondition(buttonIsPressed, buttonWasPressed)    // Modification approprie des variables buttonIsPressed et buttonWasPressed
         
         switch(s){
 
-            case INIT :
+            case INIT :                                       // INIT: DEL eteinte, augmentation du compteur, et/ou changement a EA
                 PORTC = off
                 
                 if (buttonIsPressed && !buttonWasPressed) {
@@ -52,7 +72,7 @@ int main() {
                 }
             break;
             
-            case EA   :
+            case EA   :                                        // Maintient DEL a la couleur rouge pour 1000 ms (1 seconde) puit changement a INIT
                 PORTC = red;
                 _delay_ms(ONTIME);
                 s = INIT;
