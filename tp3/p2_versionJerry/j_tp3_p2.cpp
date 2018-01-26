54e8b161891c566f0cc293944b04274333b9ea1d
Jean-Raphaël Matte <jemath@l3818-07.info.polymtl.ca>
2018-01-26 13:22:40 -0500

    Suppression d'anciens fichiers deplaces, completion du probleme 2

--- tp3/p2_versionJerry/j_tp3_p2.cpp
+++ tp3/p2_versionJerry/j_tp3_p2.cpp
@@ -3,9 +3,9 @@
  * Description: Make the LED light up in various colors as the button is
  *              pressed and released.
  * 
- *              Pinout :    LED ( - + )
+ *              Pinout :    LED ( + - )
  *                                | |
- *                       PORT C ( 0 1 2 3 4 5 6 7 8 )
+ *                       PORT C ( 0 1 2 3 4 5 6 7 )
  * 
  * Version: 1.0
  */
@@ -13,26 +13,6 @@
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
-
-#define TIMER_SIZE 4U
-
-// Time keeping structure
-struct timer {
-    uint8_t ms[TIMER_SIZE];
-};
-
-void incrementTimer (&timer t) {
-    for (int i = 0; i < TIMER_SIZE+1; i++) {
-        if (t.ms[TIMER_SIZE-(i+1)] == 0xff) {
-            t.ms[TIMER_SIZE-(i)]++;
-        }
-    }
-    t.ms[0]++;
-}
-
-uint8_t wait (uint8_t ms, uint8_t multiplier) {
-    
-}
 
 int main () {
     
@@ -46,92 +26,101 @@ int main () {
     const uint8_t DEBOUNCE_TIME = 40;
     
     // in us
-    const uint8_t LOOP_TIME = 1000;
-    
-    // Time keeping variable
-    timer time;
+    const uint8_t LOOP_TIME = 100;
     
     // Constants for the LED in port 0..1
     const uint8_t OFF   = 0x00;
     const uint8_t RED   = 0x01;
     const uint8_t GREEN = 0x02;
     
+    // List of all possible states fot the state machine
     enum state { INIT, SAMBER, SGREEN1, SRED, SOFF, SGREEN2 };
     
     state machine = INIT;
-    
-    
 
     // Infinite loop
     while (1){
         switch(machine) {
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             case INIT : 
                 
                 PORTC = RED;
                 
-                
+                // When the button is pressed
                 if (PIND & 0x04) {
-                    
-                }
-                if () {
                     machine = SAMBER;
+                    _delay_ms(DEBOUNCE_TIME);
                 }
                 
             break;
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             case SAMBER : 
                 
                 //Flips the signal sent to the LED
                 PORTC ^= 0x03;
                 
+                // When the button is released
                 if (!(PIND & 0x04)) {
                     machine = SGREEN1;
+                    
+                    // The LED must be changed before the debounce delay,
+                    // otherwise it'll flash a random color for DEBOUNCE_TIME ms
+                    PORTC = GREEN;
+                    
                     _delay_ms(DEBOUNCE_TIME);
                 }
                 
             break;
-            case SGREEN1: 
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            case SGREEN1 : 
                 
                 PORTC = GREEN;
                 
+                // When the button is pressed
                 if (PIND & 0x04) {
                     machine = SRED;
                     _delay_ms(DEBOUNCE_TIME);
                 }
                 
             break;
-            case SRED : 
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            case SRED: 
                 
                 PORTC = RED;
                 
+                // When the button is released
                 if (!(PIND & 0x04)) {
                     machine = SOFF;
                     _delay_ms(DEBOUNCE_TIME);
                 }
                 
             break;
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             case SOFF : 
                 
                 PORTC = OFF;
                 
+                // When the button is pressed
                 if (PIND & 0x04) {
                     machine = SGREEN2;
                     _delay_ms(DEBOUNCE_TIME);
                 }
                 
             break;
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             case SGREEN2 : 
                 
                 PORTC = GREEN;
                 
+                // When the button is released
                 if (!(PIND & 0x04)) {
                     machine = INIT;
                     _delay_ms(DEBOUNCE_TIME);
                 }
                 
             break;
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         }
-        
-        incrementTimer(time);
         
         _delay_us(LOOP_TIME);
     }
