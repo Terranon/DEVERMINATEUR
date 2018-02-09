344f5f07f916db0139a4689459e9f805e32f82c0
unknown <maximilien.bianchi@gmail.com>
2018-02-09 14:05:54 -0500

    MB: Finished m_tp5_p1

--- tp5/m_tp5_p1/m_tp5_p1.cpp
+++ tp5/m_tp5_p1/m_tp5_p1.cpp
@@ -14,13 +14,30 @@
 /******************************************************************************\
  * Preprocessor instructions
 \******************************************************************************/
-#include <m_tp5_p1.h>
+#include "m_tp5_p1.h"
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-volatile uint8_t etat = 0;
+ISR(INT0_VECT) {
+
+    _delay_loop_ms (30);
+
+    // se souvenir ici si le bouton est pressé ou relâché
+
+    machine++
+
+    // Voir la note plus bas pour comprendre cette instruction et son rôle
+
+    EIFR |= (1 << INTF0);
+}
+
+void initialisation() {
+
+// cli bloque toutes les interruptions.
+
+cli ();
 
 // C PORT output
 DDRC = 0xff;
@@ -28,34 +45,22 @@ DDRC = 0xff;
 // D PORT input
 DDRD = 0x00;
 
+
+// cette procédure ajuste le registre EIMSK
+
+// de l’ATmega324PA pour permettre les interruptions externes
+
+EIMSK |= (1 << INT0) ;
+
+
 // N'importe quel changement logique sur INT0 genere une requete d'interruption
+
 EICRA |= (0 << ISC01) | (1 << ISC00);
 
 
-// à prendre en charge en argument
+// sei permet de recevoir à nouveau des interruptions.
 
-ISR (INT0_VECT) {
-
-// laisser un delai avant de confirmer la réponse du
-
-// bouton-poussoir: environ 30 ms (anti-rebond)
-
-_delay_loop_ms ( 30 );
-
-// se souvenir ici si le bouton est pressé ou relâché
-
-'modifier ici'
-
-// changements d'états tels que ceux de la
-
-// semaine précédente
-
-'modifier ici'
-
-// Voir la note plus bas pour comprendre cette instruction et son rôle
-
-EIFR |= (1 << INTF0) ;
-GPIOR0 |= (!!estPese << 0);
+sei ();
 
 }
 
@@ -63,14 +68,11 @@ GPIOR0 |= (!!estPese << 0);
  * Main
 \******************************************************************************/
 
-int main () {
+int main() {
     
+    initialisation();
     
-    
-    // List of all possible states for the state machine
-    enum state { INIT, SAMBER, SGREEN1, SRED, SOFF, SGREEN2 };
-    
-    state machine = INIT;
+    machine = INIT;
 
     // Infinite loop
     while (1){
@@ -82,16 +84,6 @@ int main () {
                 
                 PORTC = RED;
                 
-                // When the button is pressed
-                if (PIND & 0x04) {
-                    
-                    // Change state
-                    machine = SAMBER;
-                    
-                    // Wait for debounce
-                    _delay_ms(DEBOUNCE_TIME);
-                }
-                
             break;
             // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             case SAMBER : 
@@ -99,35 +91,11 @@ int main () {
                 //Flips the signal sent to the LED
                 PORTC ^= 0x03;
                 
-                // When the button is released
-                if (!(PIND & 0x04)) {
-                    
-                    // Change state
-                    machine = SGREEN1;
-                    
-                    // The LED must be changed before the debounce delay,
-                    // otherwise it'll flash a random color for DEBOUNCE_TIME ms
-                    PORTC = GREEN;
-                    
-                    // Wait for debounce
-                    _delay_ms(DEBOUNCE_TIME);
-                }
-                
             break;
             // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             case SGREEN1 : 
                 
                 PORTC = GREEN;
-                
-                // When the button is pressed
-                if (PIND & 0x04) {
-                    
-                    // Change state
-                    machine = SRED;
-                    
-                    // Wait for debounce
-                    _delay_ms(DEBOUNCE_TIME);
-                }
                 
             break;
             // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -135,31 +103,11 @@ int main () {
                 
                 PORTC = RED;
                 
-                // When the button is released
-                if (!(PIND & 0x04)) {
-                    
-                    // Change state
-                    machine = SOFF;
-                    
-                    // Wait for debounce
-                    _delay_ms(DEBOUNCE_TIME);
-                }
-                
             break;
             // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             case SOFF : 
                 
                 PORTC = OFF;
-                
-                // When the button is pressed
-                if (PIND & 0x04) {
-                    
-                    // Change state
-                    machine = SGREEN2;
-                    
-                    // Wait for debounce
-                    _delay_ms(DEBOUNCE_TIME);
-                }
                 
             break;
             // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -167,21 +115,9 @@ int main () {
                 
                 PORTC = GREEN;
                 
-                // When the button is released
-                if (!(PIND & 0x04)) {
-                    
-                    // Change state
-                    machine = INIT;
-                    
-                    // Wait for debounce
-                    _delay_ms(DEBOUNCE_TIME);
-                }
-                
             break;
             // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         }
-        
-        _delay_us(LOOP_TIME);
     }
   return 0;
 }
--- tp5/m_tp5_p1/m_tp5_p1.h
+++ tp5/m_tp5_p1/m_tp5_p1.h
@@ -1,13 +1,17 @@
+// List of all possible states for the state machine
+enum etat { INIT, SAMBER, SGREEN1, SRED, SOFF, SGREEN2 };
     
+volatile etat machine;
 
+// in ms
+const uint8_t DEBOUNCE_TIME = 40;
     
-    // in ms
-    const uint8_t DEBOUNCE_TIME = 40;
+// in us
+const uint8_t LOOP_TIME = 100;
     
-    // in us
-    const uint8_t LOOP_TIME = 100;
-    
-    // Constants for the LED in port 0..1
-    const uint8_t OFF   = 0x00;
-    const uint8_t RED   = 0x01;
-    const uint8_t GREEN = 0x02;
+// Constants for the LED in port 0..1
+const uint8_t OFF   = 0x00;
+const uint8_t RED   = 0x01;
+const uint8_t GREEN = 0x02;
+
+void initialisation()
