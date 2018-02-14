121bacdbdf15a79970e1b9d60f8f68a7cf4ad55b
Jean-Raphael Matte <jean-raphael.matte@polymtl.ca>
2018-02-14 08:48:43 -0500

    JRM, Mise a jour de j_tp5_p2

--- tp5/m_tp5_p1/m_tp5_p1.cpp
+++ tp5/m_tp5_p1/m_tp5_p1.cpp
@@ -1,3 +1,22 @@
+// List of all possible states for the state machine
+enum etat { INIT, SAMBER, SGREEN1, SRED, SOFF, SGREEN2 };
+    
+volatile etat machine;
+
+// in ms
+const uint8_t DEBOUNCE_TIME = 40;
+    
+// in us
+const uint8_t LOOP_TIME = 100;
+    
+// Constants for the LED in port 0..1
+const uint8_t OFF   = 0x00;
+const uint8_t RED   = 0x01;
+const uint8_t GREEN = 0x02;
+
+void initialisation()
+
+
 /*****************************************************************************\
  * Authors:     Maximilien Bianchi, Jean-Raphael Matte
  * Name:        m_tp5_p1.cpp
@@ -14,7 +33,7 @@
 /******************************************************************************\
  * Preprocessor instructions
 \******************************************************************************/
-#include "m_tp5_p1.h"
+
 #include <avr/io.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
