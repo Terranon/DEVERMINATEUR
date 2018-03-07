e78b992e3c2a39904a78234b19bbd09ff4bc96ee
Thomas Dufour <thdufa@l3818-15.info.polymtl.ca>
2018-03-07 13:44:19 -0500

    ajout rapport makefile

--- tp8/lib-deverminateur/PhotoCell.cpp
+++ tp8/lib-deverminateur/PhotoCell.cpp
@@ -1,1 +1,64 @@
+/*****************************************************************************\
+ * Authors: Jean-Raphael Matte, Maximilien Bianchi, Thomas Dufour, William Chartrand
+ * Name: PhotoCell.cpp
+ * Description:
+ * Version: 1.0
+\******************************************************************************/
 
+#include "PhotoCell.h"
+#include "can.h"
+
+/**
+ * \brief default constructor for the intensity of the Led
+ * \param valeurIntensite_ sets the Led's color
+ * \return none
+ */
+ 
+PhotoCell::PhotoCell(uint8_t valeurIntensite_ = 0){
+}
+
+/**
+ * \brief allumerDelAmbree turns the Led to ambree
+ * \param none
+ * \return none
+ */
+ 
+void PhotoCell::allumerDelAmbree(){
+	PORTC = DEL_ROUGE;
+	_delay_ms(5);
+	PORTC = DEL_VERT;
+	_delay_ms(10);
+}
+
+/**
+ * \brief sets Led's color based on valeurIntensite_'s value. the Led's
+ * 		will keep his color as long as the intensity stays in the range
+ * \param valeurIntensite_ determines color of Led
+ * \return none
+ */
+ 
+void PhotoCell::SetCouleurLed(valeurIntensite_){
+	
+	while(1){
+		
+	valeurIntensite_ = convertisseur.lecture(position) >> 2;
+	
+	if (valeurIntensite_ < intensiteFaible)
+		PORTC = DEL_VERT;
+
+    else if (valeurIntensite_ < intensiteMoyenne)  
+		allumerDelAmbree();
+
+	else 
+		PORTC = DEL_ROUGE;
+	}
+}
+
+/**
+ * \brief destructor for the Led
+ * \param pwmFreq sets begining frequency of the pwm in Hz
+ * \return an Led
+ */
+ 
+PhotoCell::~PhotoCell{
+}
--- tp8/lib-deverminateur/PhotoCell.h
+++ tp8/lib-deverminateur/PhotoCell.h
@@ -1,1 +1,32 @@
+/*****************************************************************************\
+ * Authors: Jean-Raphael Matte, Maximilien Bianchi, Thomas Dufour, William Chartrand
+ * Name: sPhotoCell.h
+ * Description:
+ * Version: 1.0
+\******************************************************************************/
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+class PhotoCell {
+public:
+    
+PhotoCell(uint8_t valeurIntensite);
+void AllumeDelAmbre;
+void SetCouleurLed(valeurIntensite);
+can convertisseur;
+PhotoCell~;
+    
+private:
+
+uint8_t valeurIntensite_;
+const uint8_t ENTREE = 0x00;      
+const uint8_t SORTIE = 0xff;
+const uint8_t DEL_ROUGE = (1<<1);
+const uint8_t DEL_VERT = (1<<0);
+const uint8_t intensiteFaible = 140; 
+const uint8_t intensiteMoyenne = 230;
+DDRA = ENTREE;
+DDRC = SORTIE;
+
+};
