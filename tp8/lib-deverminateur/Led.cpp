a88487520c94c1ac5ca5b5132ce9c9f4c5800e86
unknown <maximilien.bianchi@gmail.com>
2018-03-08 09:51:33 -0500

    MB: changes to Led class, Piezo class preliminary work

--- tp8/lib-deverminateur/Led.cpp
+++ tp8/lib-deverminateur/Led.cpp
@@ -1,6 +1,6 @@
 /*****************************************************************************\
  * Authors:     Jean-Raphael Matte, Maximilien Bianchi
- * Name:        Led.h
+ * Name:        Led.cpp
  * Description: Simplify the use of an LED
  * Version: 2.0
 \******************************************************************************/
@@ -8,7 +8,7 @@
 #include "Led.h"
 
 /**
- * \brief constructor for the Led; the DDRx register will be set to enable
+ * \brief constructor for the Led; the DDRC register will be set to enable
  *        output on the appropriate pins
  * \param pwmFreq sets begining frequency of the pwm in Hz
  * \return an Led
@@ -16,10 +16,36 @@
 Led::Led(uint16_t pwmFreq)
     : color_(OFF),
       pwmFrequency_(pwmFreq) {
-          
-          DDRC = 0x03;              // sets PINC0 and PINC1 to output
+
+        DDRB = 0x06;              // sets PINB3 and PINB4 to output
+        
+        // TCCR0A/TCCR0B are the Timer/Counter Control Registers 0 A/B]
+    
+        // ~~~TCCR0A~~~
+        
+        TCCR0A |= (3 << COM0A0);
+        TCCR0A |= (2 << COM0B0);
+        TCCR0A |= (1 << WGM00);
+        TCCR0A &= ~(1 << WGM01);
+    
+        // ~~~TCCR0B~~~
+        TCCR0B |= (0 << FOC0A);
+        TCCR0B |= (0 << FOC0B);
+        TCCR0B &= ~(1 << WGM02);
+        TCCR0B |= (5 << CS00);
+    
+        // TIMSK0 is the Timer Interrupt Mask Register 0
+        TIMSK0 &= ~(0 << ICIEA);
+        TIMSK0 &= ~(0 << ICIEB);
+        TIMSK0 &= ~(0 << TOIE);
+        
+        TCNT0 = 0;
+*/
 }
 
+/**
+ * \brief destructor for the Led
+ */
 Led::~Led() {
 }
 
@@ -35,7 +61,7 @@ void Led::amberColor(float time) {
     
     for(uint32_t i = 0; i < nLoopIterations; i++) {
         
-        PORTC ^= color_;
+        PORTC ^= color_;            // toggles the pins of PORTC to produce an amber color
         delay_ms(10);
     }
     
@@ -93,8 +119,8 @@ void Led::setPwmColor(uint8_t color, float onTime, float offTime) {
  */
 void Led::setPwm(uint8_t color, uint16_t pwmFreq, uint32_t time, float percentOn) {
     
-    color_(color);
-    pwmFrequency_(pwmFreq);
+    color_ = color;
+    pwmFrequency_ = pwmFreq;
     
     uint32_t nLoopIterations = time / pwmFreq;
     float onTime = pwmFreq * (percentOn / 100);
@@ -127,4 +153,20 @@ void Led::toggleColor() {
     } else if (color_ == GREEN) {
         PORTC = RED;
     }
+}
+
+/**
+ * \brief gets value of color_
+ * \return a color_
+ */
+uint8_t Led::getColor() {
+    return color_;
+}
+
+/**
+ * \brief gets value of pwmFrequency_
+ * \return a pwmFrequency_
+ */
+uint16_t Led::getPwmFrequency() {
+    return pwmFrequency_;
 }
