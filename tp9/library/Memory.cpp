054862080744df2bf25f32248b72aa952396c5ce
Jean-Raphael Matte <jean-raphael.matte@polymtl.ca>
2018-03-19 19:01:30 -0400

    JRM, Ajout d'une classe Memory qui ne fait qu'utiliser Memoire24CXXX pour lire dans la EEPROM

---
+++ tp9/library/Memory.cpp
@@ -0,0 +1,24 @@
+﻿/*****************************************************************************\
+ * Authors:     Jean-Raphael Matte
+ * Name:        Memory.cpp
+ * Description: This class can read data from an external EEPROM memory over the
+ *              RS232 protocol. Currently, it is only an interface for the
+ *              Memoire24CXXX class.
+ *                                                                              
+ * Version: 1.0
+\******************************************************************************/
+
+#include "Memory.h"
+
+Memory::Memory()
+	: eeprom() {
+}
+
+Memory::~Memory() {
+}
+
+Memory::read(uint16_t address) {
+	uint8_t byteRead = 0;
+	eeprom.lecture(address, &byteRead);
+	return byteRead;
+}
