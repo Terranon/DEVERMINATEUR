fcdbd71e06b2473005c3507c115ea18c4ddb05b2
Thomas Dufour <thdufa@l3818-18.info.polymtl.ca>
2018-04-04 10:01:52 -0400

    TD: push de class sensor

--- final/library/Sensor.cpp
+++ final/library/Sensor.cpp
@@ -14,62 +14,25 @@
  * \param none
  * \return none
  */
+
 Sensor::Sensor() : convertisseur_()
 {
-	valeurDistance = 0;
-	position_ = 0;
-}
-/*
-void Sensor::setDistance(uint8_t dist){
 
-		case 10:
-			//OUTPUT 2.6V
-		case 15:
-			//OUTPUT 1.75V
-		case 20:
-			//OUTPUT 1.4V
-		case 25:
-			//OUTPUT 1.13V
-		case 30:
-			//OUTPUT 0.96V
-		case 35:
-			//OUTPUT 0.85V
-		case 40:
-			//OUTPUT 0.75V
-		case 45:
-			//OUTPUT 0.67V
-		case 50:
-			//OUTPUT 0.62V
-		case 55:
-			//OUTPUT 0.58V
-		case 60:
-			//OUTPUT 0.53V
-		case 65:
-			//OUTPUT 0.5V
-		case 70:
-			//OUTPUT 0.47V
-		case 75:
-			//OUTPUT 0.43V
-		case 80:
-			//OUTPUT 0.41V
+	position_ = 0;
+	valeurVoltage_ = 0;
 }
-*/
+
 /**
  * \brief return distance between bot and wall
  * \param valeurIntensite_ determines color of Led
  * \return none
  */
-
-uint8_t Sensor::getDistance()
+ 
+uint8_t Sensor::getValeurVoltage()
 {
 	valeurVoltage_ = convertisseur_.read(position_) >> 2;
-	uint8_t distance = (6787/(valeurVoltage_-3))-4;
-	if (distance < 10)
-		return 0;
-	if (distance > 60)
-		return 255
-	else
-		return distance;
+	
+	return valeurVoltage_;
 }
 
 
@@ -78,7 +41,9 @@ uint8_t Sensor::getDistance()
  * \param position of pin on A port
  * \return none
  */
+ /*
 void Sensor::setPosition(uint8_t position)
 {
 	position_ = position;
 }
+*/
--- final/library/Sensor.h
+++ final/library/Sensor.h
@@ -20,27 +20,32 @@ public:
  */
 	Sensor();
 
-
+/*
 	void setDistance(uint8_t dist);
-
+*/
 /**
  * \brief sets Led's color based on valeurIntensite_'s value. the Led's
  * 		will keep his color as long as the intensity stays in the range
  * \param valeurIntensite_ determines color of Led
  * \return value of intensity
  */
+ 
+	uint8_t getValeurVoltage();
+	/*
 	uint8_t getDistance();
-	
+	*/
 /**
  * \brief sets pin's position on A port
  * \param position of pin on A port
  * \return none
  */
+ /*
 	void setPosition(uint8_t position);
 	
-	
+	*/
 private:
 	uint8_t valeurVoltage_;
+
 	Adc convertisseur_;
 	uint8_t position_;
 
