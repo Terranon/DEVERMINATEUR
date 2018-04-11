d14a491b5b78758eac9297d2b7faf296e4902534
William Chartrand <wichak@l3818-05.info.polymtl.ca>
2018-04-10 22:25:18 -0400

    JRM, modification de la formule dans romgen

--- final/tools/romgen/romgen.cpp
+++ final/tools/romgen/romgen.cpp
@@ -2,27 +2,31 @@
 
 using namespace std;
 
-long distance (int reading, float aref) {
-    long rv = 5 * ((34.796 * (255/(reading*aref))-5.1197)-10);
+float readingToVolt (int reading, float aref) {
+	  return ((aref * reading) / 255);
+}
+
+long distance (float volt) {
+    long rv = (26.977 / volt) - 2.4973;
     if (rv < 0) {
         rv = 0;
-    } else if (rv > 255) {
-        rv = 255;
+    } else if (rv > 60) {
+        rv = 61;
     }
     return rv;
 }
 
 int main () {
 
-    float aref = 3.0;
+    float aref = 3.5;
     cout << "{";
     for (int i = 0; i < 255; i++) {
-        cout << distance(i, aref) << ",\t";
+        cout << distance(readingToVolt(i, aref)) << ",\t";
         if ((i+1)%8 == 0) {
             cout << "\n";
         }
     }
-    cout << distance(255, aref) << "}";
+    cout << distance(readingToVolt(255, aref)) << "}";
 
     return 0;
 }
