caf915cf658a7440ec17f554d968179fe97e97e2
William Chartrand <wichak@l3818-07.info.polymtl.ca>
2018-04-11 09:32:31 -0400

    wc : modification parcours

--- final/projects/test/src/project.cpp
+++ final/projects/test/src/project.cpp
@@ -18,76 +18,83 @@
 \******************************************************************************/
 #include "Motor.h"
 #include "Sensor.h"
+#include "Button.h"
 
 Motor bot;
-Sensor sensor;
 Button bouton;
-enum State {INIT, MUR_GAUCHE, MUR_DROIT, BOUTON_ROT_GAUCHE, BOUTON_ROT_DROIT};
 
-/******************************************************************************\
- * Init
-	Initialisation 
-\******************************************************************************/
-void init(){
-	State etat = INIT;
-}
+
+enum State {INIT, MUR_GAUCHE, MUR_DROIT, BOUTON_ROT_GAUCHE, BOUTON_ROT_DROIT};
 
 /******************************************************************************\
  * Loop
 	Boucle infini imposant au robot de rester a une distance de 15 cm du mur droit
 \******************************************************************************/
 void loop(){
-	uint8_t distanceDetecteurDroit = sensor.getDistanceG();		//distance detectee a gauche
-	uint8_t distanceDetecteurGauche = sensor.getDistanceD();	//distance detectee a droite
- 	
-	switch(etat){
+	Sensor sensor(0, 2);			//on initialise le sensor gauche a PA0 et le sensor droit a PA2
+	State etat = INIT;				//on initialise etat a INIT
+	
+	while(1){
+		uint8_t distanceDetecteurDroit = sensor.getDistanceD();		//distance detectee a droite
+		uint8_t distanceDetecteurGauche = sensor.getDistanceG();	//distance detectee a gauche
+			
+		switch(etat){
 
-		case INIT:							//detecte le mur le plus pres et va commencer a le suivre
-			if(distanceDetecteurDroit < distanceDetecteurGauche)	
-				etat = MUR_DROIT;
-			else
-				etat = MUR_GAUCHE;
-		break;
-		
-		case MUR_DROIT:						//suit un mur a droite
-			if(distanceDetecteurDroit < 15 )
-				bot.setSpeed(220, 130);
-			else
-				bot.setSpeed(130, 220);
-		break;
-		
-		case MUR_GAUCHE:					//suit un mur a gauche
-			if (distanceDetecteurGauche < 15)
-				bot.setSpeed(130, 220);
-			else
-				bot.setSpeed(220, 130);					
-		break;
+			case INIT:							//detecte le mur le plus pres et va commencer a le suivre
+				if(distanceDetecteurDroit < distanceDetecteurGauche)	
+					etat = MUR_DROIT;
+				else
+					etat = MUR_GAUCHE;
+			break;
+			
+			case MUR_DROIT:						//suit un mur a droite
+				bot.setDirection(Motor::FRWD, Motor::FRWD);
+				if(distanceDetecteurDroit < 15 )
+					bot.setSpeed(220, 130);
+				else
+					bot.setSpeed(130, 220);
+			break;
+			
+			case MUR_GAUCHE:					//suit un mur a gauche
+			  bot.setDirection(Motor::FRWD, Motor::FRWD);
+				if (distanceDetecteurGauche < 15)
+					bot.setSpeed(130, 220);
+				else
+					bot.setSpeed(220, 130);					
+			break;
 
-		case BOUTON_ROT_GAUCHE:				//effectue une rotation vers la gauche de 180 degre puis suit un mur a sa gauche
-			while (sensor.getDistanceG() == 15)
-				bot.setSpeed(0, 200);
-			etat = MUR_GAUCHE;
-		break;
 
-		case BOUTON_ROT_DROIT:				//effectue une rotation vers la droite de 180 degre puis suit un mur a sa droite
-			while (sensor.getDistanceD() == 15)
-				bot.setSpeed(200, 0);
-			etat = MUR_DROIT;
-		break;
+
+			case BOUTON_ROT_GAUCHE:				//effectue une rotation vers la gauche de 180 degre puis suit un mur a sa gauche
+				bot.setDirection(Motor::BACK, Motor::FRWD);
+				bot.setSpeed(200, 200);
+				if (distanceDetecteurGauche < 15) 
+				{
+					etat = MUR_GAUCHE;
+				}
+			break;
+
+			case BOUTON_ROT_DROIT:				//effectue une rotation vers la droite de 180 degre puis suit un mur a sa droite
+			  bot.setDirection(Motor::FRWD, Motor::BACK);
+			  bot.setSpeed(200, 200);
+				if (distanceDetecteurDroit < 15) 
+				{
+					etat = MUR_DROIT;
+				}
+			break;
+		}
+
+		if (!bouton.isPressed()){					//observe si le bouton est appuye
+			if (etat == MUR_DROIT)
+				etat = BOUTON_ROT_DROIT;
+			else
+				etat = BOUTON_ROT_GAUCHE;
+		}
 	}
-
-	if (bouton.isPressed())					//observe si le bouton est appuye
-		if (etat = MUR_DROIT)
-			etat = BOUTON_ROT_GAUCHE;
-		else
-			etat = BOUTON_ROT_DROIT;
 }
 /******************************************************************************\
  * Main
 \******************************************************************************/
 int main(){
-	init();
-	while(1){
-		loop();
-	}
+	loop();
 }
