4acbf47ab79ee46136fd7ee7a5f8bbf2b8b17368
Maximilien Bianchi <mabiab@l3818-06.info.polymtl.ca>
2018-04-11 10:03:06 -0400

    JRM, ajout d'un projet test opur le usb

---
+++ final/projects/test usb/src/project.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************\
+ * Authors:     Jean-Raphael Matte, Maximilien Bianchi, Thomas Dufour,
+ *              William Chartrand
+ * Name:        project.cpp
+ * Description: Le programme suivant impose au robot de rester a 
+				une distance de 15 cm d'un mur a droite. Nous ne prenons pas en 
+				consideration les poteaux et les fins de murs. Ce programme  
+				utilise donc les classes Sensor et Motor. De plus, ce programme 
+				permet au robot de changer de direction de maniere fluide.
+ * 
+ * Pinout :                                                     
+ *                                                                     
+ * PORTA  1 3 5 7    PORTB  1 3 5 7    PORTC  0 1 2 3 4 5 6 7    PORTD  1 3 5 7        
+ *        0 2 4 6           0 2 4 6                                     0 2 4 6       
+ *                                                                      
+
+ * Version: 1.0
+\******************************************************************************/
+#include "USBCommunicator.h"
+
+/******************************************************************************\
+ * Main
+\******************************************************************************/
+int main(){
+	USBCommunicator usbCommunicator;
+    usbCommunicator.communicate();
+}
