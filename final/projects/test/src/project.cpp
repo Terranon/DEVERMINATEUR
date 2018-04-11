0d118562928e8606471f073c8d22d2f1d949b2c0
William Chartrand <wichak@l3818-07.info.polymtl.ca>
2018-04-11 10:55:24 -0400

    wc : modifs parcours

--- final/projects/test/src/project.cpp
+++ final/projects/test/src/project.cpp
@@ -19,78 +19,120 @@
 #include "Motor.h"
 #include "Sensor.h"
 #include "Button.h"
+#include "Obstacle.h"
 
 Motor bot;
 Button bouton;
+Obstacle obstacle;
+const uint8_t DISTANCE_OPTIMALE = 15;
 
-
-enum State {INIT, MUR_GAUCHE, MUR_DROIT, BOUTON_ROT_GAUCHE, BOUTON_ROT_DROIT};
+enum State {INIT, START, MUR_GAUCHE, MUR_DROIT, ROTATION_GAUCHE, ROTATION_DROIT, TRANSITION_GAUCHE, TRANSITION_DROIT};
 
 /******************************************************************************\
  * Loop
 	Boucle infini imposant au robot de rester a une distance de 15 cm du mur droit
 \******************************************************************************/
 void loop(){
-	Sensor sensor(0, 2);			//on initialise le sensor gauche a PA0 et le sensor droit a PA2
+	Sensor sensor(2, 0);			//on initialise le sensor gauche a PA0 et le sensor droit a PA2
 	State etat = INIT;				//on initialise etat a INIT
-	
+	DDRC = 0xff;
+			
 	while(1){
 		uint8_t distanceDetecteurDroit = sensor.getDistanceD();		//distance detectee a droite
 		uint8_t distanceDetecteurGauche = sensor.getDistanceG();	//distance detectee a gauche
-			
+
 		switch(etat){
 
-			case INIT:							//detecte le mur le plus pres et va commencer a le suivre
+			case INIT:								//le robot ne commence pas avant d'avoir reussi l'epreuve initiale
+				if(distanceDetecteurDroit < distanceDetecteurGauche){
+					PORTC = 0x01;
+					if(distanceDetecteurGauche < distanceDetecteurDroit){
+						PORTC = 0x02;
+						etat = START;
+						_delay_ms(2000);
+						PORTC = 0x00;
+					}
+				}
+			break;
+			
+			case START:								//detecte le mur le plus pres et va commencer a le suivre
 				if(distanceDetecteurDroit < distanceDetecteurGauche)	
 					etat = MUR_DROIT;
 				else
-					etat = MUR_GAUCHE;
+					etat = MUR_GAUCHE;		
 			break;
 			
 			case MUR_DROIT:						//suit un mur a droite
+				PORTC = 0x01; //allume rouge si un mur a droite
 				bot.setDirection(Motor::FRWD, Motor::FRWD);
-				if(distanceDetecteurDroit < 15 )
-					bot.setSpeed(220, 130);
+				if(distanceDetecteurDroit < DISTANCE_OPTIMALE)
+					bot.setSpeed(220, 150);
 				else
-					bot.setSpeed(130, 220);
+					bot.setSpeed(150, 220);
 			break;
 			
-			case MUR_GAUCHE:					//suit un mur a gauche
+			case MUR_GAUCHE:						//suit un mur a gauche
+				PORTC = 0x02; //allume vert si un mur a gauche
 			  bot.setDirection(Motor::FRWD, Motor::FRWD);
-				if (distanceDetecteurGauche < 15)
-					bot.setSpeed(130, 220);
+				if (distanceDetecteurGauche < DISTANCE_OPTIMALE)
+					bot.setSpeed(150, 220);
 				else
-					bot.setSpeed(220, 130);					
+					bot.setSpeed(220, 150);					
 			break;
 
 
-
-			case BOUTON_ROT_GAUCHE:				//effectue une rotation vers la gauche de 180 degre puis suit un mur a sa gauche
+			case ROTATION_GAUCHE:				//effectue une rotation vers la gauche de 180 degre puis suit un mur a sa gauche
+				PORTC = 0x00;
 				bot.setDirection(Motor::BACK, Motor::FRWD);
-				bot.setSpeed(200, 200);
-				if (distanceDetecteurGauche < 15) 
+				bot.setSpeed(220, 220);
+				if (distanceDetecteurGauche < DISTANCE_OPTIMALE) 
 				{
 					etat = MUR_GAUCHE;
 				}
 			break;
 
-			case BOUTON_ROT_DROIT:				//effectue une rotation vers la droite de 180 degre puis suit un mur a sa droite
+			case ROTATION_DROIT:				//effectue une rotation vers la droite de 180 degre puis suit un mur a sa droite
+				PORTC = 0x00;
 			  bot.setDirection(Motor::FRWD, Motor::BACK);
-			  bot.setSpeed(200, 200);
-				if (distanceDetecteurDroit < 15) 
+			  bot.setSpeed(220, 220);
+				if (distanceDetecteurDroit < DISTANCE_OPTIMALE) 
 				{
 					etat = MUR_DROIT;
 				}
 			break;
+			
+			case TRANSITION_GAUCHE:			//effectue une transition vers la gauche vers un mur
+				bot.setSpeed(190, 220);
+				if(distanceDetecteurGauche < DISTANCE_OPTIMALE)
+					etat = MUR_GAUCHE;
+			break;
+			
+			case TRANSITION_DROIT:			//effectue une transition vers la droite vers un mur
+				bot.setSpeed(220, 190);
+				if(distanceDetecteurDroit < DISTANCE_OPTIMALE)
+					etat = MUR_DROIT;			
+			break;
 		}
-
-		if (!bouton.isPressed()){					//observe si le bouton est appuye
-			if (etat == MUR_DROIT)
-				etat = BOUTON_ROT_DROIT;
+		
+		//s'informe sur ce qu'il se passe avec l'autre sensor
+		uint8_t detectionObstacle = detectionObstacle();
+		if(detectionObstacle == 1)
+			etat = POTEAU;
+		if(detectionObstacle == 2){
+			if (etat == MUR_GAUCHE)
+				etat = TRANSITION_DROIT;
 			else
-				etat = BOUTON_ROT_GAUCHE;
+				etat = TRANSITION_GAUCHE;
 		}
-	}
+		
+		//s'informe si le bouton est appuye
+		if (!bouton.isPressed()){					//observe si le bouton est appuye
+			if (etat == MUR_DROIT)					//si il suivait un mur droit, il va faire une rotation sur sa gauche
+				etat = ROTATION_GAUCHE;
+			else
+				etat = ROTATION_DROIT;
+		}
+	}	
 }
 /******************************************************************************\
  * Main
