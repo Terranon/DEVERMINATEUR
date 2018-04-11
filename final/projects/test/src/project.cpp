a260b9363bcd0aae642247ef1a25630f1efcd39c
William Chartrand <william.chartrand.8@gmail.com>
2018-04-10 22:37:37 -0400

    wc : modification project.cpp final

--- final/projects/test/src/project.cpp
+++ final/projects/test/src/project.cpp
@@ -22,18 +22,14 @@
 Motor bot;
 Sensor sensor;
 Button bouton;
-enum State {INIT, MUR_GAUCHE, MUR_DROIT,BOUTON};
-
-
-
+enum State {INIT, MUR_GAUCHE, MUR_DROIT, BOUTON_ROT_GAUCHE, BOUTON_ROT_DROIT};
 
 /******************************************************************************\
  * Init
 	Initialisation 
 \******************************************************************************/
 void init(){
-	DDRA = 0x00; //port entree
-
+	State etat = INIT;
 }
 
 /******************************************************************************\
@@ -41,40 +37,50 @@ void init(){
 	Boucle infini imposant au robot de rester a une distance de 15 cm du mur droit
 \******************************************************************************/
 void loop(){
-	uint8_t distanceDetecteurDroit = sensor.getDistanceG();
-	uint8_t distanceDetecteurGauche = sensor.getDistanceD();
-	
-	State etat = INIT;
-	
+	uint8_t distanceDetecteurDroit = sensor.getDistanceG();		//distance detectee a gauche
+	uint8_t distanceDetecteurGauche = sensor.getDistanceD();	//distance detectee a droite
+ 	
 	switch(etat){
-		
-		case INIT:							//detecte le mur le plus pres et va commencer a
-			if (bouton.isPressed())
-				etat = BOUTON;
-			else if(distanceDetecteurDroit < distanceDetecteurGauche)	//le suivre
+
+		case INIT:							//detecte le mur le plus pres et va commencer a le suivre
+			if(distanceDetecteurDroit < distanceDetecteurGauche)	
 				etat = MUR_DROIT;
 			else
 				etat = MUR_GAUCHE;
 		break;
 		
-		case MUR_DROIT:
+		case MUR_DROIT:						//suit un mur a droite
 			if(distanceDetecteurDroit < 15 )
 				bot.setSpeed(220, 130);
 			else
 				bot.setSpeed(130, 220);
 		break;
 		
-		case MUR_GAUCHE:
-		 if (distanceDetecteurGauche < 15)
+		case MUR_GAUCHE:					//suit un mur a gauche
+			if (distanceDetecteurGauche < 15)
 				bot.setSpeed(130, 220);
 			else
 				bot.setSpeed(220, 130);					
 		break;
 
-		case BOUTON:
-			
-	
+		case BOUTON_ROT_GAUCHE:				//effectue une rotation vers la gauche de 180 degre puis suit un mur a sa gauche
+			while (sensor.getDistanceG() == 15)
+				bot.setSpeed(0, 200);
+			etat = MUR_GAUCHE;
+		break;
+
+		case BOUTON_ROT_DROIT:				//effectue une rotation vers la droite de 180 degre puis suit un mur a sa droite
+			while (sensor.getDistanceD() == 15)
+				bot.setSpeed(200, 0);
+			etat = MUR_DROIT;
+		break;
 	}
+
+	if (bouton.isPressed())					//observe si le bouton est appuye
+		if (etat = MUR_DROIT)
+			etat = BOUTON_ROT_GAUCHE;
+		else
+			etat = BOUTON_ROT_DROIT;
 }
 /******************************************************************************\
  * Main
